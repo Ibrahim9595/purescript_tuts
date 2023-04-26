a7896ddedc92e62ee712188583c6bc9ca237417d
Ibrahim095 <ibrahim@dalia.co>
2023-04-26 10:16:30 +0200

    Finalize ch13

--- src/Ch13.purs
+++ src/Ch13.purs
@@ -3,10 +3,12 @@ module Ch13 where
 import Data.Generic.Rep (class Generic)
 import Data.Maybe (Maybe(..))
 import Data.Show.Generic (genericShow)
+import Data.String (toUpper)
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude (class Show, Unit, discard, show, ($), (/))
+import Prelude (class Show, Unit, discard, identity, show, ($), (*), (+), (/), (<<<), (<>), (==))
 
+-- Functors
 class Functor f where
   map :: ∀ a b. (a -> b) -> (f a -> f b)
 
@@ -46,6 +48,31 @@ instance showThreeple :: (Show a, Show b, Show c) => Show (Threeple a b c) where
 instance functorThreeple :: Functor (Threeple a b) where
   map f (Threeple x y z) = Threeple x y $ f z
 
+-- Bifunctors
+class Bifunctor f where
+  bimap :: ∀ a b c d. (a -> c) -> (b -> d) -> (f a b -> f c d)
+
+instance bifunctorEither :: Bifunctor Either where 
+  bimap f _ (Left err) = Left $ f err
+  bimap _ g (Right x) = Right $ g x
+
+instance bifunctorTuple :: Bifunctor Tuple where
+  bimap f g (Tuple x y) = Tuple (f x) (g y)
+
+
+instance bifunctorThreeple :: Bifunctor (Threeple a) where
+  bimap f g (Threeple x y z) = Threeple x (f y) (g z)
+
+
+
+rmap :: ∀ f a b c. Bifunctor f => (b -> c) -> f a b -> f a c 
+rmap = bimap identity
+
+lmap :: ∀ f a b c. Bifunctor f => (a -> c) -> f a b -> f c b 
+lmap f = bimap f identity
+
 test :: Effect Unit 
 test = do
-  log $ show $ (_/2) <$> Threeple 10 20 40
+  log $ show $ rmap (_ * 2) $ Threeple 99 80 40 
+  log $ show $ lmap (_ / 2) $ Threeple 99 80 40 
+  log $ show $ bimap (_ / 2) (_ * 2) $ Threeple 99 80 40
